e555bdece12330e565b5eebc0212eaf9515b4308
kmidkiff <kmidkiff@vampeo.com>
2017-07-11 21:50:29 -0400

    Notification templates swagger update

--- api/notifications.js
+++ api/notifications.js
@@ -5,13 +5,13 @@ let Notification = require("../models/notifications");
 //Notification.createFromTemplate()
 
 module.exports = function(router){
-    //get system notijficationz
+    //get system notifications
     router.get("/notifications/system", function(req, res, next){
         Notification.findAll("user_id", null, (notifications) => {
             res.locals.json = notifications.map(notification => notification.data);
             next();
         })
-    })
+    });
     require("./entity")(router, Notification, "notifications", "user_id");
     return router;
 };
