1beba94e261faae3d36a3dea8538a24a7ea82353
kmidkiff <kmidkiff@vampeo.com>
2017-09-13 00:05:53 -0400

    New service request form updates 8

--- views/components/elements/forms/servicebot-base-field.jsx
+++ views/components/elements/forms/servicebot-base-field.jsx
@@ -17,22 +17,50 @@ const inputField = ({input, placeholder, label, type, meta: {touched, error, war
     </div>
 );
 
-const selectField = ({input, label, type, options, valueKey, labelKey, meta: {touched, error, warning}}) => (
-    <div className="form-group form-group-flex">
-        {label && <label className="control-label form-label-flex-md">{label}</label>}
-        <div className="form-input-flex">
-            <select className="form-control" {...input} placeholder={label}>
-                {options && options.map((option, index) =>
-                    <option key={index} value={valueKey ? option[valueKey] : option.id}>
-                        {labelKey ? option[labelKey] : option.name}
-                    </option>
-                    )
-                }
-            </select>
-            {touched && ((error && <span className="form-error">{error}</span>) || (warning && <span>{warning}</span>))}
-        </div>
-    </div>
-);
+class selectField extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+/*    componentDidMount(){
+        let {options, defaultValue} = this.props;
+        let self = this;
+        this.props.input.onChange(options[0]);
+
+    }*/
+    componentDidUpdate(prevProps, prevState){
+        let {options, defaultValue, input} = this.props;
+        let self = this;
+        console.log("IOTIONS", options);
+        if((!input.value || !options.find(option =>option.id == input.value)) && options.length > 0 ){
+            input.onChange(options[0].id);
+        }
+        else if(options.length == 0 && prevProps.options.length > 0){
+            input.onChange(undefined);
+        }
+    }
+
+    render() {
+        let {input, label, type, options, valueKey, labelKey, meta: {touched, error, warning}} = this.props;
+        return (
+            <div className="form-group form-group-flex">
+                {label && <label className="control-label form-label-flex-md">{label}</label>}
+                <div className="form-input-flex">
+                    <select className="form-control" {...input} placeholder={label}>
+                        {options && options.map((option, index) =>
+                            <option key={index} value={valueKey ? option[valueKey] : option.id}>
+                                {labelKey ? option[labelKey] : option.name}
+                            </option>
+                        )
+                        }
+                    </select>
+                    {touched && ((error && <span className="form-error">{error}</span>) || (warning && <span>{warning}</span>))}
+                </div>
+            </div>
+        );
+    }
+}
 
 class iconToggleField extends React.Component {
 
