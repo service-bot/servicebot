322a2bd31d15bd4b6fef7b46f359b25ef141f860
kmidkiff <kmidkiff@vampeo.com>
2017-10-10 21:04:09 -0400

    some cleanup #223

--- views/components/pages/manage-users.jsx
+++ views/components/pages/manage-users.jsx
@@ -231,7 +231,6 @@ class ManageUsers extends React.Component {
     }
     rowActionsFormatter(cell, row){
         let self = this;
-        console.log("ROW", row)
         return (
             <Dropdown
                 direction="right"
@@ -249,8 +248,6 @@ class ManageUsers extends React.Component {
                     {   type: "button",
                         label: 'Edit Role',
                         action: () => {
-                        console.log("FFFHHFHFFHHF SHAR");
-                        console.log(row)
                         return (this.openEditRole(row)) }},
                     {   type: "button",
                         label: row.status !== 'suspended' ? 'Suspend User' : 'Unsuspend User',
