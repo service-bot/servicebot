0c56818ece85d52ecdf0df3955d2acc57fa3d3ce
kalungsy <lsy@vampeo.com>
2017-10-12 21:37:06 -0400

    removed OK emojis from add fund success message

--- views/components/elements/forms/billing-settings-form.jsx
+++ views/components/elements/forms/billing-settings-form.jsx
@@ -4,7 +4,7 @@ import Fetcher from "../../utilities/fetcher.jsx"
 import {get, has} from "lodash";
 import ServiceBotBaseForm from "./servicebot-base-form.jsx";
 import {inputField} from "./servicebot-base-field.jsx";
-import { required} from 'redux-form-validators'
+import {required} from 'redux-form-validators'
 
 import {Field,} from 'redux-form'
 
@@ -13,45 +13,42 @@ class CardSection extends React.Component {
     render() {
         return (
             <div className="p-20 form-group" id="card-element">
-
-                <CardElement  style={{           base: {
-                    color: '#32325d',
-                    lineHeight: '24px',
-                    fontFamily: 'Helvetica Neue',
-                    fontSmoothing: 'antialiased',
-                    fontSize: '16px',
-                    '::placeholder': {
-                        color: '#aab7c4'
-                    }
-                },
+                <CardElement style={{
+                    base: {
+                        color: '#32325d',
+                        lineHeight: '24px',
+                        fontFamily: 'Helvetica Neue',
+                        fontSmoothing: 'antialiased',
+                        fontSize: '16px',
+                        '::placeholder': {
+                            color: '#aab7c4'
+                        }
+                    },
                     invalid: {
                         color: '#fa755a',
                         iconColor: '#fa755a'
-                    }}} />
+                    }
+                }}/>
             </div>
         );
     }
-};
+}
 
 
-class BillingForm extends React.Component{
-
-
-    render(){
-        return(
+class BillingForm extends React.Component {
+    render() {
+        return (
             <Elements id="payment-form">
                 <CreditCardForm {...this.props}/>
             </Elements>
-
         )
     }
-
 }
 
-function BillingInfo(props){
+function BillingInfo(props) {
     return (
         <form onSubmit={props.handleSubmit}>
-            <CardSection />
+            <CardSection/>
             <Field name="name" type="text" component={inputField} label="Name"/>
             <Field name="address_line1" type="text" component={inputField} label="Address Line 1"/>
             <Field name="address_line2" type="text" component={inputField} label="Address Line 2"/>
@@ -63,46 +60,46 @@ function BillingInfo(props){
 }
 
 class CreditCardForm extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            hasCard : false,
+            hasCard: false,
             loading: true,
-            card : {}
+            card: {}
         };
         this.submissionPrep = this.submissionPrep.bind(this);
 
         this.checkIfUserHasCard = this.checkIfUserHasCard.bind(this);
     }
 
-
-    componentDidMount(){
-        if(this.props.uid) {
+    componentDidMount() {
+        if (this.props.uid) {
             this.checkIfUserHasCard();
-        }else{
-            this.setState({loading : false, hasCard : false});
+        } else {
+            this.setState({loading: false, hasCard: false});
         }
 
     }
-    async submissionPrep(values){
+
+    async submissionPrep(values) {
         let token = await this.props.stripe.createToken({...values});
         console.log(token);
-        if(token.error){
+        if (token.error) {
             throw token.error.message
         }
-        return {user_id : this.props.uid , token_id : token.token.id};
+        return {user_id: this.props.uid, token_id: token.token.id};
     }
 
-    checkIfUserHasCard(){
+    checkIfUserHasCard() {
         let self = this;
         Fetcher(`/api/v1/users/${self.props.uid}`).then(function (response) {
-            if(!response.error){
-                if(has(response, 'references.funds[0]') && has(response, 'references.funds[0].source.card')){
+            if (!response.error) {
+                if (has(response, 'references.funds[0]') && has(response, 'references.funds[0].source.card')) {
                     let fund = get(response, 'references.funds[0]');
                     let card = get(response, 'references.funds[0].source.card');
                     self.setState({
                         loading: false,
-                        displayName : response.name || response.email || "You",
+                        displayName: response.name || response.email || "You",
                         hasCard: true,
                         fund: fund,
                         card: card,
@@ -113,10 +110,10 @@ class CreditCardForm extends React.Component {
                             address_city: card.address_city,
                             address_state: card.address_state,
                         }
-                    }, function(){
+                    }, function () {
                     });
                 }
-            }else{
+            } else {
                 self.setState({loading: false, hasCard: false});
             }
         });
@@ -128,38 +125,38 @@ class CreditCardForm extends React.Component {
             'url': `/api/v1/funds`
         };
 
-        let {hasCard, displayName, card : {brand, last4, exp_month, exp_year}} = this.state;
+        let {hasCard, displayName, card: {brand, last4, exp_month, exp_year}} = this.state;
         return (
-                <div id="payment-form">
-                    {hasCard &&
-                    (<div>
-                        <h3>{hasCard  ? `${displayName} have` : 'Added'} a {brand} card in your account
-                            ending in <span className="last4">{last4}</span></h3>
+            <div id="payment-form">
+                {hasCard &&
+                (<div>
+                    <h3>{hasCard ? `${displayName} have` : 'Added'} a {brand} card in your account
+                        ending in <span className="last4">{last4}</span></h3>
 
-                        <p>Expiration: <span className="exp_month">{exp_month}</span> / <span
-                            className="exp_year">{exp_year}</span></p>
+                    <p>Expiration: <span className="exp_month">{exp_month}</span> / <span
+                        className="exp_year">{exp_year}</span></p>
 
-                        <hr/>
-                        <p>Update current payment method</p>
-                    </div>)}
+                    <hr/>
+                    <p>Update current payment method</p>
+                </div>)}
 
-                    <div className="form-row">
-                        <ServiceBotBaseForm
-                            form = {BillingInfo}
-                            initialValues = {{...this.state.personalInformation}}
-                            submissionPrep = {this.submissionPrep}
-                            submissionRequest = {submissionRequest}
-                            successMessage = {"Fund added 👌🏿👌🏿👌🏿👌🏿👌🏿👌🏿👌🏿👌🏿"}
-                            handleResponse = {this.props.handleResponse}
+                <div className="form-row">
+                    <ServiceBotBaseForm
+                        form={BillingInfo}
+                        initialValues={{...this.state.personalInformation}}
+                        submissionPrep={this.submissionPrep}
+                        submissionRequest={submissionRequest}
+                        successMessage={"Fund added successfully"}
+                        handleResponse={this.props.handleResponse}
 
-                        />
+                    />
 
-                    </div>
                 </div>
+            </div>
         );
     }
 }
+
 CreditCardForm = injectStripe(CreditCardForm);
 
-
-export  {CreditCardForm, BillingForm, CardSection};
+export {CreditCardForm, BillingForm, CardSection};
