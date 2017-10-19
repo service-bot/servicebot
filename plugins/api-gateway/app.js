7dcf70ede56a2c7cb2754ed91aeb05e9f13f50c5
Ben Sears <bsears@vampeo.com>
2017-10-19 15:43:34 -0400

    CORS

--- plugins/api-gateway/app.js
+++ plugins/api-gateway/app.js
@@ -29,6 +29,13 @@ module.exports = {
         app.use(bodyParser.urlencoded({
             extended: false
         }));
+        app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Credentials", true);
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
+
         app.use(cookieParser());
         app.use(express.static(path.join(__dirname, '../../public')));
         let servers = createServer(appConfig, app);
