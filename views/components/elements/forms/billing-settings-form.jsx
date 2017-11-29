ee2ea874b316fd4f0c60ff4c1d027d9142b0e026
shar <sd@microsharweb.com>
2017-11-28 22:07:23 -0500

    Fixed billing form bug

--- views/components/elements/forms/billing-settings-form.jsx
+++ views/components/elements/forms/billing-settings-form.jsx
@@ -102,25 +102,27 @@ class CreditCardForm extends React.Component {
                 }
             }, function () {
             });
-            if(card) {
-                self.setState({
-                    showForm: true
-                });
-            }
         } else {
-            self.setState({loading: false, hasCard: false});
+            self.setState({
+                loading: false,
+                hasCard: false,
+                showForm: true
+            });
         }
-
     }
 
     async submissionPrep(values) {
         let token = await this.props.stripe.createToken({...values});
         console.log(token);
         if (token.error) {
+            let message = token.error;
+            if(token.error.message) {
+                message = token.error.message;
+            }
             this.setState({ alerts: {
                 type: 'danger',
                 icon: 'times',
-                message: token.error
+                message: message
             }});
             throw token.error
         }
@@ -141,10 +143,10 @@ class CreditCardForm extends React.Component {
                             fund: fund,
                             card: card,
                             personalInformation: {
-                                name: card.name,
-                                address_line1: card.address_line1,
-                                address_city: card.address_city,
-                                address_state: card.address_state,
+                                name: card.name || "",
+                                address_line1: card.address_line1 || "",
+                                address_city: card.address_city || "",
+                                address_state: card.address_state || "",
                             }
                         }, function () {
                         });
