8c658d7a63b88de0ed08dd0a98981703839de82e
bsears90 <bsears@vampeo.com>
2018-04-10 19:32:12 -0400

    Added accepted header

--- plugins/api-gateway/app.js
+++ plugins/api-gateway/app.js
@@ -33,7 +33,7 @@ module.exports = {
         app.use(function(req, res, next) {
             res.header("Access-Control-Allow-Credentials", true);
             res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
             next();
         });
 
