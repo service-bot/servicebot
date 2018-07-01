65919c911c173729bf6239f43d7d04eae36aad5d
kmidkiff <kmidkiff@vampeo.com>
2018-07-01 13:29:36 -0400

    fixes

--- config/default-notifications.js
+++ config/default-notifications.js
@@ -5,7 +5,7 @@ default_notifications.templates = [
         event_name: "service_instance_cancellation_requested",
         message: "Your cancellation request for [[name]] has been submitted. You will receive another notification when it has been approved.",
         subject: "Service cancellation submitted",
-        description: "Triggered when a service cancellation has been requested by a user",
+        description: "Sent when a service cancellation has been requested by a user",
         model: "service-instance",
         send_email: true
     },
@@ -14,7 +14,7 @@ default_notifications.templates = [
         event_name: "service_instance_updated",
         message: "Your subscription to [[name]] has been updated.",
         subject: "Subscription updated",
-        description: "Triggered when a service has been updated",
+        description: "Sent when a service has been updated",
         model: "service-instance",
         send_email: true
     },
@@ -23,7 +23,7 @@ default_notifications.templates = [
         event_name: "service_instance_cancellation_approved",
         message: "Your cancellation request has been approved.",
         subject: "Service cancellation request approved",
-        description: "Triggered when a service cancellation request has been approved",
+        description: "Sent when a service cancellation request has been approved",
         model: "service-instance-cancellation",
         send_email: true
     },
@@ -32,7 +32,7 @@ default_notifications.templates = [
         event_name: "service_instance_cancellation_rejected",
         message: "Your cancellation request has been rejected. ",
         subject: "Service cancellation request rejected",
-        description: "Triggered when a service cancellation request has been rejected",
+        description: "Sent when a service cancellation request has been rejected",
         model: "service-instance-cancellation",
         send_email: true
     },
@@ -41,7 +41,7 @@ default_notifications.templates = [
         event_name: "password_reset_request_created",
         message: "Follow the following link to <a href='[[url]]'>reset your password</a>.",
         subject: "ServiceBot password reset",
-        description: "Triggered when a user requests a password reset",
+        description: "Sent when a user requests a password reset",
         model: "user",
         send_email: true
     },
@@ -50,7 +50,7 @@ default_notifications.templates = [
         event_name: "user_registered",
         message: "You have gained a new user! [[name]] - [[email]] has signed up.",
         subject: "New User",
-        description: "Triggered for an admin when a new user has signed up",
+        description: "Sent for an admin when a new user has signed up",
         model: "user",
         send_email: true,
         send_to_owner: false
@@ -60,7 +60,7 @@ default_notifications.templates = [
         event_name: "user_suspended",
         message: "Your account has been suspended for payment failure.",
         subject: "Account Suspended",
-        description: "Triggered when an account has been suspended",
+        description: "Sent when an account has been suspended",
         model: "user",
         send_email: true
     },
@@ -69,7 +69,7 @@ default_notifications.templates = [
         event_name: "payment_failure",
         message: "Your payment failed. Please log into your account and check your billing settings.",
         subject: "Service Payment Failure",
-        description: "Triggered when a payment has failed to go through. It notifies the user to update their payment method",
+        description: "Sent when a payment has failed to go through. It notifies the user to update their payment method",
         model: "user",
         send_email: true
     },
@@ -77,7 +77,7 @@ default_notifications.templates = [
         event_name:"user_invited",
         message:"Hello, \r\nYou have been invited to use the [[_company_name]] ServiceBot System. Please click the link to begin user <a href='[[url]]'>registration</a>.",
         subject:"ServiceBot Invitation!",
-        description:"This notification it triggered when a user is invited to they system by an admin",
+        description:"Sent when a user is invited to they system by an admin",
         model:"user",
         send_email:true
     },
