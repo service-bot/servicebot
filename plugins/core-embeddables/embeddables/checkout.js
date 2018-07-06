dadd1da4c587a9602ed72201d125ece3f4baebc2
kmidkiff <kmidkiff@vampeo.com>
2018-07-06 16:48:00 -0400

    Remove 'machine name'

--- plugins/core-embeddables/embeddables/checkout.js
+++ plugins/core-embeddables/embeddables/checkout.js
@@ -37,12 +37,9 @@ class CustomField extends React.Component {
             willAutoFocus, index, typeValue, member, myValues, privateValue, requiredValue, promptValue, configValue,
             setPrivate, setRequired, setPrompt, changePrivate, changeRequired, changePrompt, templateType
         } = props;
-        let machineName;
 
         if (myValues.prop_label) {
             willAutoFocus = false;
-            machineName = slug(myValues.prop_label, {lower: true});
-
         }
         return (
             <div className="custom-property-fields">
@@ -101,12 +98,6 @@ class CustomField extends React.Component {
                     }
                 </div>
                 <div id="custom-prop-widget" className="custom-property-field-group">
-                    {machineName &&
-                    <div className="form-group form-group-flex addon-options-widget-config-input-wrapper">
-                        <label className="control-label form-label-flex-md addon-options-widget-config-input-label">Machine
-                            Name</label>
-                        <pre>{machineName}</pre>
-                    </div>}
                     {typeValue && <RenderWidget
                         showPrice={(templateType !== "custom" && templateType !== "split")}
                         member={member}
