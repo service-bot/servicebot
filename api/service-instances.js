a3d3b00de31d085257f08cbb01f4c51995eb4c2a
bsears90 <bsears@vampeo.com>
2018-07-10 15:39:13 -0400

    fixed issue with errors if customer no fund

--- api/service-instances.js
+++ api/service-instances.js
@@ -110,7 +110,11 @@ module.exports = function(router) {
             store.dispatchEvent("service_instance_updated", updatedInstance);
         }).catch(function (error) {
             console.error(error);
-            res.json({error});
+            if(error.message === "This customer has no attached payment source"){
+                res.status(400).json({error : error.message});
+            }else {
+                res.status(500).json({error});
+            }
         });
     });
 
