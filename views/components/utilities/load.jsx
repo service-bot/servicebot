26683013a1b0d58fc4377d8427286a8e228c8e48
kalungsy <lsy@vampeo.com>
2018-07-14 16:11:45 -0400

    update loader style and adds delay prop

--- views/components/utilities/load.jsx
+++ views/components/utilities/load.jsx
@@ -7,12 +7,23 @@ class Load extends React.Component {
         this.state = {
             type: this.props.type || 'content',
             message: "Loading...",
-            loadState: "loading"
+            loadState: "loading",
+            show: false,
         };
     }
 
     componentDidMount() {
         let self = this;
+        let {timeout, delayed, show} = this.props;
+
+        if(delayed !== false){
+            this.delayed = setTimeout(function () {
+                self.setState({
+                    show: true
+                })
+            }, 1000)
+        }
+
         if(this.props.timeout !== false ){
             this.timeout = setTimeout(function(){
                 self.setState({message: "There seems to be a problem in processing your request. Please try again.", loadState: "done" });
@@ -22,46 +33,50 @@ class Load extends React.Component {
 
     componentWillUnmount() {
         clearTimeout(this.timeout);
+        clearTimeout(this.delayed);
     }
 
     render () {
 
-        let style={};
-        let loadingStyle={};
-        if (this.state.type == 'content' || this.state.type == 'dataform'){
-            if(this.state.loadState == 'loading'){
-                loadingStyle={
-                    position: 'absolute',
-                    top: '50%',
-                    left: '47%',
-                    transform: 'translate(-50%,-50%)',
-                    height: '80px',
-                    width: '80px',
-                    zIndex: 999999
-                };
-            }
-        }else if(this.state.type == 'button'){
-            if(this.state.loadState == 'loading'){
-                loadingStyle={
-                    height: '20px',
-                    width: '20px'
-                };
-            }
-        }else if(this.state.type == 'avatar'){
-            if(this.state.loadState == 'loading'){
-                loadingStyle={
-                    height: '83px',
-                    width: '83px'
-                };
-            }
+        let {timeout, delayed, show} = this.state;
+        // let style={};
+        // let loadingStyle={};
+        // if (this.state.type == 'content' || this.state.type == 'dataform'){
+        //     if(this.state.loadState == 'loading'){
+        //         loadingStyle={
+        //             position: 'absolute',
+        //             top: '50%',
+        //             left: '47%',
+        //             transform: 'translate(-50%,-50%)',
+        //             height: '80px',
+        //             width: '80px',
+        //             zIndex: 999999
+        //         };
+        //     }
+        // }else if(this.state.type == 'button'){
+        //     if(this.state.loadState == 'loading'){
+        //         loadingStyle={
+        //             height: '20px',
+        //             width: '20px'
+        //         };
+        //     }
+        // }else if(this.state.type == 'avatar'){
+        //     if(this.state.loadState == 'loading'){
+        //         loadingStyle={
+        //             height: '83px',
+        //             width: '83px'
+        //         };
+        //     }
+        // }
+        if(show){
+            return(
+                <div className="loader"><div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div></div>
+            );
+        }else{
+            return(
+                <div/>
+            )
         }
-
-        return(
-            <div className="loader" style={style}>
-                <div className={this.state.loadState} style={loadingStyle}></div>
-                <p className={`help-block m-b-0 ${this.state.loadState}`}>{this.state.message}</p>
-            </div>
-        );
     }
 }
 
