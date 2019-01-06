3706237692ce3fdb825f99900829145bbe1d301b
Ka Lung Sy <kalungsy@gmail.com>
2019-01-05 21:32:38 -0500

    email template edit form clean up.

--- views/components/elements/forms/notification-template-form.jsx
+++ views/components/elements/forms/notification-template-form.jsx
@@ -1,21 +1,19 @@
-
-import React from 'react';
-import {browserHistory} from 'react-router';
-import Load from '../../utilities/load.jsx';
+import React from 'react'
+import {browserHistory} from 'react-router'
+import Load from '../../utilities/load.jsx'
 import {Fetcher} from "servicebot-base-form"
-import {Authorizer, isAuthorized} from "../../utilities/authorizer.jsx";
-import Jumbotron from "../../layouts/jumbotron.jsx";
-import Content from "../../layouts/content.jsx";
-import {DataForm, DataChild, DataInput} from "../../utilities/data-form.jsx";
-import {Wysiwyg, WysiwygTemplater} from "../wysiwyg.jsx";
-import TagsInput from 'react-tagsinput';
-import 'react-tagsinput/react-tagsinput.css';
-import Buttons from "../buttons.jsx";
+import {Authorizer, isAuthorized} from "../../utilities/authorizer.jsx"
+import Content from "../../layouts/content.jsx"
+import {DataForm} from "../../utilities/data-form.jsx"
+import {WysiwygTemplater} from "../wysiwyg.jsx"
+import 'react-tagsinput/react-tagsinput.css'
+import Buttons from "../buttons.jsx"
+import { Section } from "../../layouts/section.jsx"
 
 class NotificationTemplateForm extends React.Component {
 
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             loading : true,
             template : {},
@@ -23,29 +21,29 @@ class NotificationTemplateForm extends React.Component {
             roleUrl : "/api/v1/roles",
             roles : [],
             success: false
-        };
+        }
 
-        this.handleFiles = this.handleFiles.bind(this);
-        this.handleResponse = this.handleResponse.bind(this);
-        this.handleRole = this.handleRole.bind(this);
-        this.insertString = this.insertString.bind(this);
-        this.handleResetSuccess = this.handleResetSuccess.bind(this);
+        this.handleFiles = this.handleFiles.bind(this)
+        this.handleResponse = this.handleResponse.bind(this)
+        this.handleRole = this.handleRole.bind(this)
+        this.insertString = this.insertString.bind(this)
+        this.handleResetSuccess = this.handleResetSuccess.bind(this)
     }
 
     componentDidMount() {
         if(!isAuthorized({permissions:"can_administrate"})){
-            return browserHistory.push("/login");
+            return browserHistory.push("/login")
         } else {
-            this.fetchData();
+            this.fetchData()
         }
     }
 
     fetchData(){
-        let self = this;
+        let self = this
         Fetcher(self.state.url).then(function(response){
             if(!response.error){
 
-                return response;
+                return response
             }
         //then get the roles
         }).then(function(r){
@@ -53,21 +51,21 @@ class NotificationTemplateForm extends React.Component {
                 Fetcher(self.state.url + "/roles").then(function(roles){
                     if(!roles.error) {
                         Fetcher(self.state.roleUrl).then(function(allRoles){
-                            let roleIds = roles.map(role => role.data.id);
-                            self.setState({loading: false, template: r, templateRoles: roles, allRoles:allRoles, roles: roleIds});
+                            let roleIds = roles.map(role => role.data.id)
+                            self.setState({loading: false, template: r, templateRoles: roles, allRoles:allRoles, roles: roleIds})
                         })
                     }
 
                 })
             }
-        });
+        })
 
 
         }
     handleRole(id){
-        let self = this;
+        let self = this
         return function(e) {
-            const target = e.target;
+            const target = e.target
             if (target.checked) {
                 if (!self.state.roles.includes(id)){
                     self.setState({roles: self.state.roles.concat(id)})
@@ -78,143 +76,135 @@ class NotificationTemplateForm extends React.Component {
 
     }}
     handleFiles(e){
-        e.preventDefault();
+        e.preventDefault()
     }
 
     handleResponse(response){
-        let self = this;
+        let self = this
         Fetcher(this.state.url + "/roles", "PUT", this.state.roles).then(function(response2){
-            self.setState({success: true});
-        });
+            self.setState({success: true})
+        })
     }
 
     handleResetSuccess(){
-        let self = this;
-        self.setState({success: false});
+        let self = this
+        self.setState({success: false})
     }
 
     handleImage(url, id){
         //This function is unused
-        var self = this;
+        var self = this
     }
 
 
     insertString(html) {
-        let self = this;
+        let self = this
         return function (e){
-            e.preventDefault();
-            self.refs.wysiwygTemplater.refs.wysiwyg.insert(html);
+            e.preventDefault()
+            self.refs.wysiwygTemplater.refs.wysiwyg.insert(html)
         }
     }
 
     humanString(text){
-        return(text.replace(/_/g, ' '));
+        return(text.replace(/_/g, ' '))
     }
 
     render() {
 
         if(this.state.loading) {
-            return <Load/>;
+            return <Load/>
         }else{
-            let template = this.state.template;
-            let roles = this.state.templateRoles;
-            let allRoles = this.state.allRoles;
-            let references = template.schema.references;
+            let template = this.state.template
+            let roles = this.state.templateRoles
+            let allRoles = this.state.allRoles
+            let references = template.schema.references
 
             return(
-                <div>
+                <React.Fragment>
                     <Authorizer permissions={["can_administrate", "can_manage"]}>
-                        <div className="page-service-instance">
+                        <div className="app-content __manage-system-settings">
                             <Content>
-                                <div className="service-instance-section">
-                                    <span className="service-instance-section-label"><strong>Editing Notification Template</strong></span>
-                                    <h4>{template.data.name}</h4>
+                                <div className={`_title-container`}>
+                                    <h1 className={`_heading`}>Edit Notification Template</h1>
                                 </div>
-                                <div className="service-instance-section">
-                                    <span className="service-instance-section-label"><strong>Select the roles you want this email to be sent to</strong></span>
-                                    <span className="help-block">The notification will be sent to all users with the roles.</span>
-                                {allRoles.map(role => {
-
-                                    let checked = roles.some(function(checkedRole){
-                                        return role.id == checkedRole.data.id
-                                    });
-
-
-                                    return (
-                                        <div key={role.id}>
-                                            <input onChange={this.handleRole(role.id)} type="checkbox" defaultChecked={checked}/>
-                                            <span> {role.role_name}</span>
-                                            <span>{role.role_name == "user" && " - email will be sent to all users"}</span>
-                                        </div>
-                                    )
-                                })}
-                                </div>
-                                <DataForm handleResponse={this.handleResponse} url={this.state.url} method="PUT">
-                                    <div className="service-instance-section notification-settings-section">
-                                        <span className="service-instance-section-label"><strong>Notification Settings</strong></span>
-
+                                <Section>
+                                    <h3>Email template name</h3>
+                                    <p>{template.data.name}</p>
+                                    <h3>Description</h3>
+                                    <p>{template.data.description}</p>
+                                </Section>
+                                <Section>
+                                            <h3>The notification will be sent to all users with the roles.</h3>
+                                            {allRoles.map(role => {
+                                                let checked = roles.some(function(checkedRole){
+                                                    return role.id === checkedRole.data.id
+                                                })
+                                                return (
+                                                    <React.Fragment key={role.id}>
+                                                        <input onChange={this.handleRole(role.id)} type="checkbox" defaultChecked={checked}/>
+                                                        <span> {role.role_name}</span>
+                                                        <span>{role.role_name === "user" && " - email will be sent to all users"}</span>
+                                                    </React.Fragment>
+                                                )
+                                            })}
+                                </Section>
+                                <Section>
+                                    <DataForm handleResponse={this.handleResponse} url={this.state.url} method="PUT">
+                                        <h3>Notification Settings</h3>
                                         <input name="create_notification" type="checkbox" defaultChecked={template.data.create_notification}/> <span>Create Notification</span><br/>
                                         <input name="send_email" type="checkbox" defaultChecked={template.data.send_email}/> <span className="inline"> Send Email</span><br/>
                                         <input name="send_to_owner" type="checkbox" defaultChecked={template.data.send_to_owner}/> <span className="inline"> Send Email To Owner</span>
+                                    </DataForm>
+                                </Section>
+                                <Section>
+                                    <h3>Email Content</h3>
+                                    <h4>Subject</h4>
+                                    <input type="text" name="subject" defaultValue={template.data.subject}/>
+                                    <h4>Body</h4>
+                                    <WysiwygTemplater receiveValue={true} receiveOnChange={true} name="message" defaultValue={template.data.message} ref="wysiwygTemplater" schema={template.schema}/>
+                                    <div className="p-t-15">
+                                        <Buttons btnType="primary" text="Save Notification Template" type="submit" loading={this.state.ajaxLoad} success={this.state.success} reset={this.handleResetSuccess}/>
+                                        <div className="clearfix"/>
                                     </div>
-                                    {/*<div className="service-instance-section">*/}
-                                        {/*<span className="service-instance-section-label"><strong>Additional Recipients</strong></span>*/}
-                                        {/*<span className="help-block">Add recipients directly, these will be people who will also get this email notification for this event.</span>*/}
-                                        {/*<TagsInput  name="additional_recipients" receiveOnChange={true} receiveValue={true} defaultValue={template.data.additional_recipients || []}/>*/}
-                                    {/*</div>*/}
+                                </Section>
+                                <Section>
                                     <div className="service-instance-section">
-                                        <span className="service-instance-section-label"><strong>Description</strong></span>
-                                        <p>{template.data.description}</p>
-                                    </div>
-                                    <div className="service-instance-section">
-                                        <span className="service-instance-section-label"><strong>Subject</strong></span>
-                                        <input type="text" name="subject" defaultValue={template.data.subject}/>
-                                    </div>
-                                    <div className="service-instance-section">
-                                        <span className="service-instance-section-label"><strong>Body</strong></span>
-                                        <WysiwygTemplater receiveValue={true} receiveOnChange={true} name="message" defaultValue={template.data.message} ref="wysiwygTemplater" schema={template.schema}/>
-                                        <div className="p-t-15">
-                                            <Buttons btnType="primary" text="Save Notification Template" type="submit" loading={this.state.ajaxLoad} success={this.state.success} reset={this.handleResetSuccess}/>
-                                            <div className="clearfix"/>
-                                        </div>
-                                    </div>
-                                </DataForm>
-                                <div className="service-instance-section">
-                                    <span className="service-instance-section-label"><strong>Data Fields</strong></span>
-                                    <span className="help-block">Available data fields, you can insert data fields related to this event ({this.humanString(template.data.name)}) into the body of your notification.</span>
-                                    <ul className = "templateList">
-                                        <span className="help-block text-capitalize">{this.humanString(template.data.name)} Fields</span>
-                                        {Object.keys(template.schema).map(field => {
-                                            if(field == "references"){
-                                                return Object.keys(references).map(reference => {
+                                        <span className="service-instance-section-label"><strong>Data Fields</strong></span>
+                                        <span className="help-block">Available data fields, you can insert data fields related to this event ({this.humanString(template.data.name)}) into the body of your notification.</span>
+                                        <ul className = "templateList">
+                                            <span className="help-block text-capitalize">{this.humanString(template.data.name)} Fields</span>
+                                            {Object.keys(template.schema).map(field => {
+                                                if(field === "references"){
+                                                    return Object.keys(references).map(reference => {
+                                                        return (
+                                                            <ul key={reference} className="referenceList list-group">
+                                                                <span className="help-block text-capitalize">{this.humanString(reference)} Fields</span>
+                                                                {Object.keys(references[reference]).map(referenceColumn => {
+                                                                    return (
+                                                                        <li key={referenceColumn} className="column reference-column list-unstyled">
+                                                                            <button className="buttons btn-sm btn-info" onClick={this.insertString(`[[references.${reference}.${referenceColumn}]]`)}>{referenceColumn}</button>
+                                                                        </li>)
+                                                                })}
+                                                            </ul>
+                                                        )
+                                                    })
+                                                }else{
                                                     return (
-                                                        <ul key={reference} className="referenceList list-group">
-                                                            <span className="help-block text-capitalize">{this.humanString(reference)} Fields</span>
-                                                            {Object.keys(references[reference]).map(referenceColumn => {
-                                                                return (
-                                                                <li key={referenceColumn} className="column reference-column list-unstyled">
-                                                                    <button className="buttons btn-sm btn-info" onClick={this.insertString(`[[references.${reference}.${referenceColumn}]]`)}>{referenceColumn}</button>
-                                                                </li>)
-                                                            })}
-                                                        </ul>
+                                                        <div>
+                                                            <li key={field} className="column list-unstyled">
+                                                                <button className="buttons btn-sm btn-info" onClick={this.insertString(`[[${field}]]`)}>{field}</button>
+                                                            </li>
+                                                        </div>
                                                     )
-                                                })
-                                            }else{
-                                                return (
-                                                    <div>
-                                                        <li key={field} className="column list-unstyled">
-                                                            <button className="buttons btn-sm btn-info" onClick={this.insertString(`[[${field}]]`)}>{field}</button>
-                                                        </li>
-                                                    </div>
-                                                )
-                                            }
-                                        })}
-                                    </ul>
-                                </div>
+                                                }
+                                            })}
+                                        </ul>
+                                    </div>
+                                </Section>
                             </Content>
                         </div>
                     </Authorizer>
-                </div>);
+                </React.Fragment>)
         }
     }
 
